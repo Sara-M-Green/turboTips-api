74a73db650cbc582b9c0daaa9eca4752f91e1f6c
Sara Green <smg105c@gmail.com>
2021-02-17 13:14:22 -0800

    refactoring tips router

--- src/tips/tips-router.js
+++ src/tips/tips-router.js
@@ -5,7 +5,6 @@ const tipsRouter = express.Router()
 const jsonParser = express.json
 
 const types = require('pg').types
-
 types.setTypeParser(1700, 'text', parseFloat);
 
 tipsRouter
@@ -18,7 +17,21 @@ tipsRouter
             })
             .catch(next)
     })
-    
+    .post(jsonParser, (req, res, next) => {
+        const { tip_date, emp_id, bottles, tips } = req.body
+        const newTipObject = { tip_date, emp_id, bottles, tips }
+        TipsService.addDailyTips(
+            req.app.get('db'),
+            newTipObject
+        )
+            .then(tipObject => {
+                res
+                    .status(201)
+                    .location(`/tips/${tipObject.tip_date}`)
+                    .json(tipObject)
+            })
+            .catch(next)
+    })
 
 tipsRouter
     .route('/:date')
@@ -35,5 +48,6 @@ tipsRouter
             })
             .catch(next)
     })
+    
 
 module.exports = tipsRouter
