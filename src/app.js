dcfce3a834fafb885a4c4b85150ba15b67382d61
Sara Green <smg105c@gmail.com>
2021-02-24 12:21:09 -0800

    cors issue

--- src/app.js
+++ src/app.js
@@ -17,11 +17,12 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny' 
     : 'common';
 
+app.use(cors())
+app.options('*', cors())
 app.use(morgan(morganOption))
 app.use(helmet())
 // app.use(cors({ origin:'http://localhost:3000' }))
-// app.use(cors())
-app.options('*', cors())
+
 
 
 types.setTypeParser(1700, 'text', parseFloat);
